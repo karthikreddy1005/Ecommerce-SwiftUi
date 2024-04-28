02c47d8336bb14b3b5f07b1d31f2ec297018e71a
karthik-reddy <karthikb10005@gmail.com>
2024-04-28 21:32:26 +0530

    initial commit

---
+++ furniture-swift/furniture-swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  furniture-swift
+//
+//  Created by Karthik Reddy on 28/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ furniture-swift/furniture-swift/furniture_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  furniture_swiftApp.swift
+//  furniture-swift
+//
+//  Created by Karthik Reddy on 28/04/24.
+//
+
+import SwiftUI
+
+@main
+struct furniture_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
